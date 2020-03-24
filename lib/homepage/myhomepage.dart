088259f4835020dfe29050e98f2f88ab3e033304
Lansional <cbg1720@gmail.com>
2020-03-24 13:51:19 +0900

    add drawer commit

--- lib/homepage/myhomepage.dart
+++ lib/homepage/myhomepage.dart
@@ -8,61 +8,98 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var _scaffoldKey = new GlobalKey<ScaffoldState>();
+
+  _drawer() {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+
+        ],
+      ),
+    );
+  }
+
+  _cardToEngTap() {
+    return Container(
+      width: 350,
+      height: 150,
+      margin: EdgeInsets.only(top: 50),
+      child: InkWell(
+        onTap: () {
+
+        },
+        child: Card(
+          child: Center(
+            child: Text(
+              '한글보고 영어 쓰기',
+              style: TextStyle(
+                fontSize: 35
+              ),
+            ),
+          ),
+          elevation: 10,
+        ),
+      )
+    );
+  }
+
+  _cardToKorTap() {
+    return Container(
+      width: 350,
+      height: 150,
+      child: InkWell(
+        onTap: () {
+
+        },
+        child: Card(
+          child: Center(
+            child: Text(
+              '영어보고 한글 쓰기',
+              style: TextStyle(
+                fontSize: 35
+              ),
+            ),
+          ),
+          elevation: 10,
+        ),
+      )
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       backgroundColor: Colors.green,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-                width: 350,
-                height: 150,
-                child: InkWell(
-                  onTap: () {
-
-                  },
-                  child: Card(
-                    child: Center(
-                      child: Text(
-                        '한글보고 영어 쓰기',
-                        style: TextStyle(
-                          fontSize: 30
-                        ),
-                      ),
-                    ),
-                    elevation: 10,
-                  ),
-                )
-              ),
-            SizedBox(
-              height: 15,
-            ),
-            Container(
-              width: 350,
-              height: 150,
-              child: InkWell(
-                onTap: () {
-                  
-                },
-                child: Card(
-                  child: Center(
-                    child: Text(
-                      '영어보고 한글 쓰기',
-                      style: TextStyle(
-                        fontSize: 30
-                      ),
-                    ),
-                  ),
-                  elevation: 10,
-                ),
-              )
+      drawer: _drawer(),
+      body: Stack(
+        children: <Widget>[
+          Positioned(
+            top: 30,
+            child: IconButton(
+              color: Colors.white,
+              icon: Icon(Icons.menu), 
+              onPressed: () {
+                _scaffoldKey.currentState.openDrawer();
+              }
             )
-          ],
-        ),
-      ),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                _cardToEngTap(),
+                SizedBox(
+                  height: 20,
+                ),
+                _cardToKorTap()
+              ],
+            ),
+          ),
+        ],
+      )
     );
   }
 }
