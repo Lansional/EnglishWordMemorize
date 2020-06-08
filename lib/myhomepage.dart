57e731bdf007e7fddd870888605e16cc0623571f
Lansional <cbg1720@gmail.com>
2020-06-09 00:00:15 +0900

    Bottomnavigationbar is so weird in our app.

--- lib/myhomepage.dart
+++ lib/myhomepage.dart
@@ -1,9 +1,5 @@
-import 'package:english_word_memorize/homepage/calendarpage.dart';
-import 'package:english_word_memorize/homepage/settingpage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
-import 'homepage/pagecontrol.dart';
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key}) : super(key: key);
@@ -13,47 +9,156 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  // bottomNaviagtionBar index
-  int _bottomIndex = 0;
+  final _key = new GlobalKey<ScaffoldState>();
+  
+  double _cardWidth = 850;      // 넓이
+  double _cardHeight = 400;     // 높이
+  double _cardTextSize = 85;    // 글꼴 크기
 
-  // bottomNaviagtionBar item
-  final _bottomItems = <BottomNavigationBarItem>[
-    BottomNavigationBarItem(
-      icon: Icon(Icons.home),
-      title: Text('홈'),
-    ),
-    BottomNavigationBarItem(
-      icon: Icon(Icons.calendar_today),
-      title: Text('춣석 캘린더')
-    ),
-    BottomNavigationBarItem(
-      icon: Icon(Icons.settings),
-      title: Text('설정')
-    )
-  ];
+  double _everyButtonHeight = 45;  // 버튼 간격
 
-  final _pageBody = <Widget>[
-    PageControl(),
-    CalendarPage(),
-    SettingPage()
-  ];
+  // Write in English for Korean report.
+  _cardToEngTap() {
+    return Container(
+      width: _cardWidth.w,
+      height: _cardHeight.h,
+      margin: EdgeInsets.only(top: 50),
+      child: RaisedButton(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(80.w)
+          )
+        ),
+        onPressed: () {
+          print('한글보고 영어 쓰기');
+        },
+        elevation: 10,
+        child: Text('한글보고 영어 쓰기' , style: TextStyle(fontSize: _cardTextSize.sp)),
+      )
+    );
+  }
+
+  // Write in Korean for English report
+  _cardToKorTap() {
+    return Container(
+      width: _cardWidth.w,
+      height: _cardHeight.h,
+      child: RaisedButton(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(80.w)
+          )
+        ),
+        onPressed: () {
+          print('영어보고 한글 쓰기');
+        },
+        elevation: 10,
+        child: Text('영어보고 한글 쓰기' , style: TextStyle(fontSize: _cardTextSize.sp)),
+      )
+    );
+  }
+
+  _vocabularyNote() {
+    return Container(
+      width: _cardWidth.w,
+      height: _cardHeight.h,
+      child: RaisedButton(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(80.w)
+          )
+        ),
+        onPressed: () {
+          print('단어장');
+        },
+        elevation: 10,
+        child: Text('단어장' , style: TextStyle(fontSize: _cardTextSize.sp)),
+      ),
+    );
+  }
+
+  // PageDrawer
+  _pageDrawer() {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          UserAccountsDrawerHeader(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage('https://i.pinimg.com/originals/2d/6b/9d/2d6b9d7145b42697d47c4fc11e7fa2ca.jpg'),
+                fit: BoxFit.cover
+              )
+            ),
+            currentAccountPicture: CircleAvatar(
+              backgroundImage: NetworkImage('https://simpleicon.com/wp-content/uploads/account.png'),
+            ),
+            accountName: Text('null'),
+            accountEmail: Text('null')
+          ),
+          ListTile(
+            title: Text('캘린더'),
+            trailing: Icon(Icons.chevron_right),
+            onTap: () {
+
+            },
+          ),
+          ListTile(
+            title: Text('설정'),
+            trailing: Icon(Icons.chevron_right),
+            onTap: () {
+
+            },
+          ),
+        ],
+      )
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    ScreenUtil.init(context, allowFontScaling: true);           // width : 1080px, height : 1920px
+    ScreenUtil.init(context);
 
     return Scaffold(
+      key: _key,
+      drawer: _pageDrawer(),
       backgroundColor: Colors.green,
-      body: _pageBody[_bottomIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _bottomIndex,
-        items: _bottomItems,
-        onTap: (index) {
-          setState(() {
-            this._bottomIndex = index;
-          });
-        },
-      ),
+      body: Stack(
+        children: <Widget>[
+          Align(
+            alignment: Alignment.topLeft,
+            child: Padding(
+              padding: EdgeInsets.only(top: 50.w),
+              child: IconButton(
+                icon: Icon(Icons.menu),
+                color: Colors.white,
+                onPressed: () {
+                  _key.currentState.openDrawer();
+                }
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  _cardToEngTap(),
+                  SizedBox(
+                    height: _everyButtonHeight.h,
+                  ),
+                  _cardToKorTap(),
+                  SizedBox(
+                    height: _everyButtonHeight.h,
+                  ),
+                  _vocabularyNote()
+                ],
+              ),
+            ),
+          )
+        ],
+      )
     );
   }
 }
