93009a1d36f1279e90771513a530946aa505badd
Lansional <cbg1720@gmail.com>
2020-06-23 22:49:11 +0900

    startPage complete

--- lib/startPage.dart
+++ lib/startPage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
 class StartPage extends StatefulWidget {
   StartPage({Key key}) : super(key: key);
 
@@ -8,6 +10,8 @@ class StartPage extends StatefulWidget {
 }
 
 class _StartPageState extends State<StartPage> {
+  final double size = 420;
+
   @override
   void initState() {
     super.initState();
@@ -20,9 +24,28 @@ class _StartPageState extends State<StartPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).primaryColor,
+      backgroundColor: Colors.green,
       body: Center(
-        child: Text('asdfasdf'),
+        child: Container(
+          height: size.h,
+          width: size.w,
+          child: Card(
+            color: Theme.of(context).primaryColor,
+            child: Center(
+              child: Text(
+                '영어           단어장',
+                textAlign: TextAlign.center,
+                maxLines: 2,
+
+                style: TextStyle(
+                  fontSize: 90.sp,
+                  color: Colors.white
+                ),
+              ),
+            ),
+            elevation: 10,
+          ),
+        ),
       ),
     );
   }
