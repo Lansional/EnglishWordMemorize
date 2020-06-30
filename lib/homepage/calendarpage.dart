118116e4f55a197a9e5e3a307de268b7a157c0ca
Lansional <cbg1720@gmail.com>
2020-06-30 19:32:14 +0900

    add calendar to compelete alpha 1

--- lib/homepage/calendarpage.dart
+++ lib/homepage/calendarpage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class CalendarPage extends StatefulWidget {
   CalendarPage({Key key}) : super(key: key);
@@ -8,16 +9,64 @@ class CalendarPage extends StatefulWidget {
 }
 
 class _CalendarPageState extends State<CalendarPage> {
+  final Map<DateTime, List> _holidays = {
+    DateTime(2019, 1, 1): ['New Year\'s Day'],
+    DateTime(2019, 1, 6): ['Epiphany'],
+    DateTime(2019, 2, 14): ['Valentine\'s Day'],
+    DateTime(2019, 4, 21): ['Easter Sunday'],
+    DateTime(2019, 4, 22): ['Easter Monday'],
+  };
+
+  Map<DateTime, List> _events;
+  List _selectedEvents;
+  CalendarController _calendarController = new CalendarController();
+
+  void _onDaySelected(DateTime day, List events) {
+    print('CALLBACK: _onDaySelected');
+    setState(() {
+      _selectedEvents = events;
+    });
+  }
+
+  void _onVisibleDaysChanged(
+      DateTime first, DateTime last, CalendarFormat format) {
+    print('CALLBACK: _onVisibleDaysChanged');
+  }
+
+  void _onCalendarCreated(
+      DateTime first, DateTime last, CalendarFormat format) {
+    print('CALLBACK: _onCalendarCreated');
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('캘린더'),
-      ),
-      body: Container(
-        child: null,
-      )
-    );
+        appBar: AppBar(
+          title: Text('캘린더'),
+        ),
+        body: TableCalendar(
+          calendarController: _calendarController,
+          events: _events,
+          holidays: _holidays,
+          // locale: 'ko_KR',
+          startingDayOfWeek: StartingDayOfWeek.monday,
+          calendarStyle: CalendarStyle(
+            selectedColor: Colors.deepOrange[400],
+            todayColor: Colors.deepOrange[200],
+            markersColor: Colors.brown[700],
+            outsideDaysVisible: false,
+          ),
+          headerStyle: HeaderStyle(
+            formatButtonTextStyle:
+                TextStyle().copyWith(color: Colors.white, fontSize: 15.0),
+            formatButtonDecoration: BoxDecoration(
+              color: Colors.deepOrange[400],
+              borderRadius: BorderRadius.circular(16.0),
+            ),
+          ),
+          onDaySelected: _onDaySelected,
+          onVisibleDaysChanged: _onVisibleDaysChanged,
+          onCalendarCreated: _onCalendarCreated,
+        ));
   }
 }
